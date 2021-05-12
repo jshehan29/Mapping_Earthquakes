19be8ab9b4eb6b95564a96abc0aa9842593098a4
Joshua Shehan <shehanjosh@gmail.com>
2021-05-12 10:59:03 -0400

    Updating logic.js file to map single point

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
 // An alternative to the code above
 // Useful when multiple tile layers are needed, or a background image of the map
@@ -13,6 +13,33 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //     ],
 //     zoom: 4
 // });
+
+// --------------------------------------------------------
+// 13.4.1 - Add markers to the map
+// Add a marker to the map for Los Angeles, California
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// Add a circle to the map
+// L.circle([34.0522, -118.2437], {
+//     radius: 100
+// }).addTo(map);
+
+// Skill drill - add yellow circle with black outline to dark map
+// circle should have 300m radius
+// L.circle([34.0522, -118.2437], {
+//     radius: 300,
+//     color: "black",
+//     fillColor: '#ffffa1'
+// }).addTo(map);
+
+// Can also add a circle marker
+L.circleMarker([34.0522, -118.2437],{
+    radius: 300,
+    color: "black",
+    fillColor: '#ffffa1'
+}).addTo(map);
+// --------------------------------------------------------
+
 
 // We create the tile layer that will be the background of our map
 // let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -27,7 +54,7 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 // streets.addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
